fc2b5b80372a5c6b1394c1b531ff5c564f18295a
Michael Magarik <mmagarik@gmail.com>
2024-09-26 16:55:20 +0400

    some placeholders

--- src/routes/dashboard/index.jsx
+++ src/routes/dashboard/index.jsx
@@ -9,6 +9,13 @@ const Dashboard = () => {
 	return (
 		<div className={styles.cont}>
 			<span className='material-symbols-outlined'>dashboard_2</span>
+			<div>Usage history...</div>
+			<div>Current status...</div>
+			<button data-primary>Start</button>
+			<div>normal link</div>
+			<div>output only link</div>
+			<div>input only link</div>
+			<div>control only link</div>
 		</div>
 	)
 }
--- src/routes/profile/index.jsx
+++ src/routes/profile/index.jsx
@@ -3,12 +3,16 @@
  * Profile
  *
  */
+import { useAuth0 } from '@auth0/auth0-react'
 import styles from './index.module.scss'
 
 const Profile = () => {
+	const { user } = useAuth0()
+
 	return (
 		<div className={styles.cont}>
 			<span className='material-symbols-outlined'>account_circle</span>
+			<div>{user.email}</div>
 		</div>
 	)
 }
